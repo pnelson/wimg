0f209fa5af1051419f3659e326ecadca80ec9554
Philip Nelson <me@pnelson.ca>
2015-08-29 12:34:04 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# wimg
+
+I have some file naming conventions that I use for saving wallpaper images.
+The file names that I choose to use include the image width and height but it
+is kind of tedious to do manually. Why do it manually when a computer can do it
+for you?
+
+## Example
+
+```
+$ wimg -name="google now mountains" https://i.imgur.com/UfCjC8q.jpg
+  => ~/img/1920x1080_google_now_mountains.jpg
+```
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"unicode"
+
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+
+	_ "image/gif"
+	_ "image/png"
+)
+
+var errHomeNotFound = errors.New("wimg: user home directory not found")
+
+func remove(r rune) bool {
+	return unicode.Is(unicode.Mn, r)
+}
+
+func normalize(name string) string {
+	t := transform.Chain(norm.NFD, transform.RemoveFunc(remove), norm.NFC)
+	name = strings.TrimSpace(name)
+	name, _, err := transform.String(t, name)
+	if err != nil {
+		panic(err)
+	}
+	name = strings.ToLower(name)
+	name = strings.Replace(name, " ", "_", -1)
+	return name
+}
+
+func run(src, name string, quality int) error {
+	home := os.Getenv("HOME")
+	if home == "" {
+		home = os.Getenv("USERPROFILE")
+	}
+	if home == "" {
+		return errHomeNotFound
+	}
+	resp, err := http.Get(src)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	m, _, err := image.Decode(resp.Body)
+	if err != nil {
+		return err
+	}
+	bounds := m.Bounds()
+	if name == "" {
+		ext := path.Ext(src)
+		name = path.Base(src)
+		name = strings.TrimSuffix(name, ext)
+	}
+	name = normalize(name)
+	name = fmt.Sprintf("%dx%d_%s.jpg", bounds.Dx(), bounds.Dy(), name)
+	filename := filepath.Join(home, "img", name)
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return jpeg.Encode(f, m, nil)
+}
+
+func main() {
+	var (
+		name    = flag.String("name", "", "name of the image")
+		quality = flag.Int("quality", 100, "jpeg quality encoding")
+	)
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 1 {
+		fmt.Fprintln(os.Stderr, "wimg: invalid arguments")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	err := run(args[0], *name, *quality)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+}
